71ae382d243b6d5c7af094f6550142dc252f4d07
bigmikeee <msc568@stern.nyu.edu>
2022-07-14 13:12:28 -0400

    Add new Python file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # rock-paper-scissors-exercise
 Repository for Assignment 1
+# Rock, Paper, Scissors Game
+This Rock, Paper, Scissors Game is the same game that you know and love. You will play against a computer that makes a choice with the help of a random word generator.
+
+# Usage
+
+No additional installation necessary
+
+```bash
+conda activate my-game-env
+conda create -n my-game-env python=3.8
+python game.py
+```
+
+# Contact
+Please email chen.michael@nyu.edu with any issues/concerns.
---
+++ game.py
@@ -0,0 +1,31 @@
+# Rock, Paper, Scissors Exercise
+
+import random
+
+#Game description and creating input box for user and randomized computer choice
+print("Welcome Player to the Rock, Paper, Scissors Game!")
+player_choice = str(input("Please input a move: 'Rock', 'Paper', or 'Scissors':\n")).title()
+computer_choice_list =  ["Rock", "Paper", "Scissors"]
+computer_choice = random.choice(computer_choice_list)
+print(f"\nYou chose {player_choice}, computer chose {computer_choice}.\n")
+
+#Prints results based on user choice and randomized computer choice
+if player_choice == computer_choice:
+    print("Tie!")
+elif player_choice == "Rock":
+  if computer_choice == "Paper":
+    print("You lose!", computer_choice, "beats", player_choice)
+  else:
+    print("You win!", player_choice, "beats", computer_choice)
+elif player_choice == "Paper":
+    if computer_choice == "Scissors":
+      print("You lose!", computer_choice, "beats", player_choice)
+    else:
+      print("You win!", player_choice, "beats", computer_choice)
+elif player_choice == "Scissors":
+    if computer_choice == "Rock":
+        print("You lose...", computer_choice, "beats", player_choice)
+    else:
+        print("You win!", player_choice, "beats", computer_choice)
+else:
+      print("That's not a valid play. Please re-run and type a valid input!")
